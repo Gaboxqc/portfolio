3078c366ca50e9305de1636422d91d7153607b4e
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2026-04-22 04:31:22 -0600

    Finish hero mobile desing

--- src/components/lineButton/LineButton.jsx
+++
@@ -1,7 +0,0 @@
-const LineButton = (props) => {
-    return(
-        <button className={"h-13 w-auto px-6 bg-transparent outline-1 rounded-lg"}>{props.label}</button>
-    )
-}
-
-export default LineButton
