94d6839a545167217e27a5f32d91354fdc224d69
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2026-05-02 01:20:03 -0600

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home.jsx";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
   return (
     <>
-      <Home/>
+        <Home/>
+        <Analytics/>
     </>
   )
 }
